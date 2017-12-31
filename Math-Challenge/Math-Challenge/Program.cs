8ee62324b99785dd91e5675a7e3e338b383aca85
dani.dearmond <dani.dearmond@gmail.com>
2017-12-30 21:36:59 -0500

    final commit with completed project

--- Math-Challenge/Math-Challenge/Program.cs
+++ Math-Challenge/Math-Challenge/Program.cs
@@ -8,7 +8,7 @@ namespace Math_Challenge
 {
     class Program
     {
-        public List<int> MakeArray(string myString)
+        private List<int> MakeList(string myString)
         {
             char[] charArray = myString.ToCharArray();
             int[] intArray = Array.ConvertAll(charArray, c => (int)Char.GetNumericValue(c));
@@ -16,31 +16,107 @@ namespace Math_Challenge
             intList.AddRange(intArray);
             return intList;
         }
+        private int ValidateInput(string myString)
+        {
+            int value;
+            if (int.TryParse(myString, out int num))
+            {
+                if (num > 0)
+                {
+                    value = num;
+                    return value;
+                }
+                else
+                {
+                    value = 0;
+                    return value;
+                }
+            }
+            else
+            {
+                value = 0;
+                return value;
+            }
+        }
 
         static void Main(string[] args)
         {
             Program n = new Program();
-            Console.WriteLine("Please input the first non-negative integer");
-            string num1 = (Console.ReadLine());
-            List<int> digits1 = new List<int>(n.MakeArray(num1));
+            //declaring variables
+            bool repeat1, repeat2;
+            string num1, num2;
+            int myValue1, myValue2;
+            char doAgain = 'Y';
 
-            Console.WriteLine("Please input the second non-negative integer");
-            string num2 = (Console.ReadLine());
-            List<int> digits2 = new List<int>(n.MakeArray(num2));
+            repeat2 = true;
+            while (repeat2)
+            {
+                //initiating variables
+                num1 = "";
+                num2 = "";
+                repeat1 = true;
 
-            Console.WriteLine(Environment.NewLine);
-            Console.WriteLine("The digits in num1 are: ");
-            foreach (int i in digits1)
-            {
-                Console.Write(i + " ");
-            }
-            Console.WriteLine(Environment.NewLine);
-            Console.WriteLine("The digits in num2 are: ");
-            foreach (int i in digits2)
-            {
-                Console.Write(i + " ");
-            }
-            Console.Read();
+                while (repeat1)
+                {
+                    Console.WriteLine("Please input the first non-negative integer");
+                    num1 = (Console.ReadLine());
+
+                    Console.WriteLine("Please input the second non-negative integer");
+                    num2 = (Console.ReadLine());
+
+                    myValue1 = (n.ValidateInput(num1));
+                    myValue2 = (n.ValidateInput(num2));
+
+                    if ((myValue1 > 0) & (myValue2 > 0))
+                    {
+                        if (num1.Length == num2.Length)
+                        {
+                            repeat1 = false;
+                        }
+                        else
+                        {
+                            Console.Write("Your integers are not the same length. Please try again.");
+                            Console.WriteLine(Environment.NewLine);
+                            repeat1 = true;
+                        }
+                    }
+                    else
+                    {
+                        Console.Write("At least one of your integers is not positive. Please try again.");
+                        Console.WriteLine(Environment.NewLine);
+                        repeat1 = true;
+                    }
+                }
+
+                List<int> digits1 = new List<int>(n.MakeList(num1));
+                List<int> digits2 = new List<int>(n.MakeList(num2));
+
+                List<int> digits3 = digits1.Zip(digits2, (item1, item2) => item1 + item2).ToList();
+
+
+                if (digits3.All(i => i == digits3[0]))
+                {
+                    Console.WriteLine(Environment.NewLine);
+                    Console.WriteLine("True");
+                }
+                else
+                {
+                    Console.WriteLine(Environment.NewLine);
+                    Console.WriteLine("False");
+                }
+
+                Console.WriteLine("Would you like to run this program again? (Y or N)");
+                doAgain = (Convert.ToChar(Console.ReadLine()));
+
+                if (doAgain == 'Y' || doAgain == 'y')
+                {
+                    continue;
+                }
+                else
+                {
+                    Environment.Exit(0);
+                }
+            }           
         }
     }
 }
